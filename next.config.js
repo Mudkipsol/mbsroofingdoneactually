4ab838fc5911492592e1d33b2dfae0a1ae738d77
Mudkipsol <ecomhamson@gmail.com>
2025-09-08 22:40:28 -0400

    Add Next.js configuration files

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**'
+      }
+    ]
+  }
+};
+module.exports = nextConfig
